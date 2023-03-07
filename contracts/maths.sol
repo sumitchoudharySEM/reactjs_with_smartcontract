930d0ac90542411b6b9afcc3b6dd0f1ccad90560
sumitchoudharySEM <sumitsem2004@gmail.com>
2023-03-07 12:52:53 +0530

    hardhat errors comes out

---
+++ contracts/maths.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.19;
+
+contract maths {
+
+    uint public myNum = 22;
+
+    function changeNum() external {
+        myNum = 33;
+    }
+
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,11 @@
-require("@nomicfoundation/hardhat-toolbox");
+import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.18",
+solidity = "0.8.19";
+networks = {
+  goerli: {
+    url: process.env.PROVIDER_URL,
+    accounts: [`0x${process.env.PRIVATE_KEY}`]
+  }
 };
